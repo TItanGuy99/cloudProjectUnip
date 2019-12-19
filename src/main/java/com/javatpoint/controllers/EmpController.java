fe70cb2999edcba7d1a180126b38b7ec994d170c
TitanGuy99 <luiz.neosnk@gmail.com>
2019-12-18 21:47:37 -0300

    Adicionando arquivos de UML.

--- src/main/java/com/javatpoint/controllers/EmpController.java
+++ src/main/java/com/javatpoint/controllers/EmpController.java
@@ -12,46 +12,42 @@ import com.javatpoint.dao.EmpDao;
 @Controller  
 public class EmpController {  
     @Autowired  
-    EmpDao dao;//will inject dao from xml file  
-      
-    /*It displays a form to input data, here "command" is a reserved request attribute 
-     *which is used to display object data into form 
-     */  
+    EmpDao dao; // Injeta o dao do arquivo xml  */
+          
+    /* Exibe o form para que recebe as informações  */
+    
     @RequestMapping("/empform")  
     public String showform(Model m){  
     	m.addAttribute("command", new Emp());
     	return "empform"; 
     }  
-    /*It saves object into database. The @ModelAttribute puts request data 
-     *  into model object. You need to mention RequestMethod.POST method  
-     *  because default request is GET*/  
+    /* Salva no database */  
     @RequestMapping(value="/save",method = RequestMethod.POST)  
     public String save(@ModelAttribute("emp") Emp emp){  
         dao.save(emp);  
         return "redirect:/viewemp";//will redirect to viewemp request mapping  
     }  
-    /* It provides list of employees in model object */  
+    /* Mostra a lista de funcionarios */  
     @RequestMapping("/viewemp")  
     public String viewemp(Model m){  
         List<Emp> list=dao.getEmployees();  
         m.addAttribute("list",list);
         return "viewemp";  
     }  
-    /* It displays object data into form for the given id.  
-     * The @PathVariable puts URL data into variable.*/  
+    /* Mostra a informação de um funcionario no form usando o ID */  
     @RequestMapping(value="/editemp/{id}")  
     public String edit(@PathVariable int id, Model m){  
         Emp emp=dao.getEmpById(id);  
         m.addAttribute("command",emp);
         return "empeditform";  
     }  
-    /* It updates model object. */  
+    /* Atualiza o objeto */  
     @RequestMapping(value="/editsave",method = RequestMethod.POST)  
     public String editsave(@ModelAttribute("emp") Emp emp){  
         dao.update(emp);  
         return "redirect:/viewemp";  
     }  
-    /* It deletes record for the given id in URL and redirects to /viewemp */  
+    /* Deleta um funcionario usando o seu ID */  
     @RequestMapping(value="/deleteemp/{id}",method = RequestMethod.GET)  
     public String delete(@PathVariable int id){  
         dao.delete(id);  
